488600756710210814545df957f7ff4b61c4db26
francisigoe <frankisigoe@gmail.com>
2026-05-31 18:44:00 +0100

    added leafnodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 class HTMLNode:
-    def __init__(self, tag: str | None = None, value:str|None=None, children:list|None=None, props:dict|None=None):
+    def __init__(self, tag: str|None =None, value: str|None =None, children: list|None =None, props: dict|None =None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -15,4 +15,23 @@ class HTMLNode:
         return result[:-1]
 
     def __repr__(self):
-        print(f"tag:{self.tag} value:{self.value} children:{self.children} props:{self.props}")
+        print(f"tag:{self.tag} value:{self.value} children:{self.children} props:{self.props}") 
+
+class LeafNode(HTMLNode):
+    def __init__(self, value: str|None, tag: str|None, props: dict|None =None):
+        super().__init__(value,tag,props = props)
+        
+    def to_html(self):
+        if not self.value:
+            raise ValueError
+        if not self.tag:
+            return self.value
+        if self.props:
+            props = ""
+            for value in self.props:
+                props = props.join(f"{value}=\"{self.props[value]}\" ")
+            return f"<{self.tag} {props[:-1]}>{self.value}</{self.tag}>" 
+        return f"<{self.tag}>{self.value}</{self.tag}>" 
+
+    def __repr__(self):
+        print(f"tag:{self.tag} value:{self.value} props:{self.props}") 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,7 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
         node = HTMLNode(props = {
     "href": "https://www.google.com",
@@ -15,5 +15,13 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node.props_to_html(), node2.props_to_html())
         self.assertNotEqual(node.props_to_html(), node3.props_to_html())
         self.assertNotEqual(node2.props_to_html(), node3.props_to_html())
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        node2  = LeafNode("a", "Whatup Earth?")
+        node3  = LeafNode("a", "Whatup Earth?", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        self.assertEqual(node2.to_html(), "<a>Whatup Earth?</a>")
+        self.assertEqual(node3.to_html(), "<a href=\"https://www.google.com\">Whatup Earth?</a>")
 if __name__ == "__main__":
     unittest.main()
